20b499d73f7df04d0ab8c9dc972b4fbaaa089a23
hoantn <67941798+hoantn@users.noreply.github.com>
2025-10-19 12:04:14 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
 import vue from '@vitejs/plugin-vue'
+import fs from 'fs'
+
+// >>> Update these paths to your Apache SSL cert & key if different
+const SSL_KEY  = 'D:/xampp/apache/conf/ssl.key/server.key'
+const SSL_CERT = 'D:/xampp/apache/conf/ssl.crt/server.crt'
 
 export default defineConfig({
   plugins: [
@@ -11,7 +16,16 @@ export default defineConfig({
     vue(),
   ],
   server: {
-    https: false,
-    host: 'mmo.homes'
-  }
+    https: {
+      key: fs.readFileSync(SSL_KEY),
+      cert: fs.readFileSync(SSL_CERT),
+    },
+    host: 'mmo.homes',
+    port: 5173,
+    hmr: {
+      host: 'mmo.homes',
+      protocol: 'wss',
+      port: 5173,
+    },
+  },
 })
